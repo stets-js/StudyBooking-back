49e42b6b5ba4f7e3b547e06013688e0055147496
monyshchenko.laptop <kekymber@gmail.com>
2024-04-07 16:33:24 +0300

    addded casscade deletion

--- models/relation.js
+++ models/relation.js
@@ -1,166 +1,169 @@
 const Sequelize = require('sequelize');
 const User = require('./user.model');
 const Role = require('./role.model');
-const {Course, TeacherCourse} = require('./course.model');
-const {Slot, Appointment_Type} = require('./slot.model');
+const { Course, TeacherCourse } = require('./course.model');
+const { Slot, Appointment_Type } = require('./slot.model');
 const SubGroup = require('./subgroup.model');
 const Replacement = require('./replacement.model');
-const {TeacherType} = require('./teacher-type.model');
+const { TeacherType } = require('./teacher-type.model');
 
 User.belongsTo(Role);
 Role.hasMany(User);
 
-Slot.belongsTo(Appointment_Type, {foreignKey: 'appointmentTypeId'});
-Appointment_Type.hasMany(Slot, {foreignKey: 'appointmentTypeId'});
+Slot.belongsTo(Appointment_Type, { foreignKey: 'appointmentTypeId' });
+Appointment_Type.hasMany(Slot, { foreignKey: 'appointmentTypeId' });
 
-User.hasMany(Slot, {foreignKey: 'userId'});
-Slot.belongsTo(User, {foreignKey: 'userId'});
+User.hasMany(Slot, { foreignKey: 'userId', onDelete: 'CASCADE' });
+Slot.belongsTo(User, { foreignKey: 'userId' });
 
 User.belongsToMany(Course, {
-  through: TeacherCourse,
-  foreignKey: 'userId',
-  otherKey: 'courseId',
-  as: 'teachingCourses'
+ through: TeacherCourse,
+ foreignKey: 'userId',
+ otherKey: 'courseId',
+ as: 'teachingCourses',
+ onDelete: 'CASCADE',
 });
 
 Course.belongsToMany(User, {
-  through: TeacherCourse,
-  foreignKey: 'userId',
-  otherKey: 'courseId',
-  as: 'teachers'
+ through: TeacherCourse,
+ foreignKey: 'courseId',
+ otherKey: 'userId',
+ as: 'teachers',
+ onDelete: 'CASCADE',
 });
+Course.belongsTo(User, { as: 'teamLead', foreignKey: 'teamLeadId' });
+User.hasMany(Course, { foreignKey: 'teamLeadId' });
 
-Course.belongsTo(User, {as: 'teamLead', foreignKey: 'teamLeadId'});
-User.hasMany(Course, {foreignKey: 'teamLeadId'});
+TeacherType.hasMany(TeacherCourse, { foreignKey: 'TeacherTypeId' });
+TeacherCourse.belongsTo(TeacherType, {
+ foreignKey: 'TeacherTypeId',
+});
+User.hasMany(SubGroup, { foreignKey: 'adminId', as: 'AdminSubGroups' });
+SubGroup.belongsTo(User, { foreignKey: 'adminId', as: 'Admin' });
 
-TeacherType.hasMany(TeacherCourse, {foreignKey: 'TeacherTypeId'});
-TeacherCourse.belongsTo(TeacherType, {foreignKey:{name: 'TeacherTypeId',type:Sequelize.INTEGER, defaultValue:1}});
-User.hasMany(SubGroup, {foreignKey: 'adminId', as: 'AdminSubGroups'});
-SubGroup.belongsTo(User, {foreignKey: 'adminId', as: 'Admin'});
-
-User.hasMany(SubGroup, {foreignKey: 'mentorId', as: 'MentorSubGroups'});
-Replacement.belongsTo(User, {foreignKey: 'mentorId', as: 'Mentor'});
+User.hasMany(Replacement, { foreignKey: 'mentorId', as: 'MentorSubGroups', onDelete: 'CASCADE' });
+Replacement.belongsTo(User, { foreignKey: 'mentorId', as: 'Mentor' });
 
 Course.hasMany(SubGroup);
 SubGroup.belongsTo(Course);
 
-SubGroup.hasMany(Slot, {onDelete: 'CASCADE'});
+SubGroup.hasMany(Slot, { onDelete: 'CASCADE' });
 Slot.belongsTo(SubGroup);
 
-Replacement.hasMany(Slot, {onDelete: 'CASCADE'});
+Replacement.hasMany(Slot, { onDelete: 'CASCADE' });
 Slot.belongsTo(Replacement);
 
 Replacement.belongsTo(SubGroup);
-SubGroup.hasMany(Replacement, {onDelete: 'CASCADE'});
+SubGroup.hasMany(Replacement, { onDelete: 'CASCADE' });
 
-User.beforeFind(async options => {
-  options.attributes = options.attributes || {};
-  options.attributes.exclude = options.attributes.exclude || [];
-  options.attributes.exclude.push('createdAt', 'updatedAt');
-  options.include = options.include || [];
-  options.include.push({
-    model: Role,
-    attributes: ['id', 'name']
-  });
+User.beforeFind(async (options) => {
+ options.attributes = options.attributes || {};
+ options.attributes.exclude = options.attributes.exclude || [];
+ options.attributes.exclude.push('createdAt', 'updatedAt');
+ options.include = options.include || [];
+ options.include.push({
+  model: Role,
+  attributes: ['id', 'name'],
+ });
 });
 
-Replacement.beforeFind(async options => {
-  options.attributes = options.attributes || {};
-  options.attributes.exclude = options.attributes.exclude || [];
+Replacement.beforeFind(async (options) => {
+ options.attributes = options.attributes || {};
+ options.attributes.exclude = options.attributes.exclude || [];
 
-  options.include = options.include || [];
-  options.include.push({
+ options.include = options.include || [];
+ options.include.push({
+  model: User,
+  as: 'Mentor',
+  attributes: ['name'],
+  foreignKey: 'mentorId',
+ });
+ options.include.push({
+  model: SubGroup,
+  attributes: ['id', 'name', 'description', 'adminId', 'CourseId'],
+  include: [
+   {
     model: User,
-    as: 'Mentor',
+    as: 'Admin',
     attributes: ['name'],
-    foreignKey: 'mentorId'
-  });
-  options.include.push({
+    foreignKey: 'adminId',
+   },
+   {
+    model: Course,
+    attributes: ['name', 'id'],
+   },
+  ],
+ });
+});
+
+Slot.beforeFind(async (options) => {
+ options.attributes = options.attributes || {};
+ options.attributes.exclude = options.attributes.exclude || [];
+ options.attributes.exclude.push('createdAt', 'updatedAt');
+ options.include = options.include || [];
+ options.include.push(
+  {
+   model: Appointment_Type,
+   attributes: ['id', 'name'],
+  },
+  {
+   model: SubGroup,
+   attributes: ['id', 'name', 'description', 'startDate', 'endDate', 'link', 'schedule'],
+   include: [
+    Course,
+    {
+     model: User,
+     as: 'Admin',
+     attributes: ['name'],
+     foreignKey: 'adminId',
+    },
+   ],
+  },
+  {
+   model: Replacement,
+   attributes: ['id', 'schedule', 'description'],
+   include: {
     model: SubGroup,
-    attributes: ['id', 'name', 'description', 'adminId', 'CourseId'],
     include: [
-      {
-        model: User,
-        as: 'Admin',
-        attributes: ['name'],
-        foreignKey: 'adminId'
-      },
-      {
-        model: Course,
-        attributes: ['name', 'id']
-      }
-    ]
-  });
-});
-
-Slot.beforeFind(async options => {
-  options.attributes = options.attributes || {};
-  options.attributes.exclude = options.attributes.exclude || [];
-  options.attributes.exclude.push('createdAt', 'updatedAt');
-  options.include = options.include || [];
-  options.include.push(
-    {
-      model: Appointment_Type,
-      attributes: ['id', 'name']
-    },
-    {
-      model: SubGroup,
-      attributes: ['id', 'name', 'description', 'startDate', 'endDate', 'link', 'schedule'],
-      include: [
-        Course,
-        {
-          model: User,
-          as: 'Admin',
-          attributes: ['name'],
-          foreignKey: 'adminId'
-        }
-      ]
-    },
-    {
-      model: Replacement,
-      attributes: ['id', 'schedule', 'description'],
-      include: {
-        model: SubGroup,
-        include: [
-          {model: Course},
-          {
-            model: User,
-            as: 'Admin',
-            attributes: ['name'],
-            foreignKey: 'adminId'
-          }
-        ]
-      }
-    }
-  );
-});
-
-SubGroup.beforeFind(async options => {
-  options.attributes = options.attributes || {};
-  options.attributes.exclude = options.attributes.exclude || [];
-  options.attributes.exclude.push('createdAt', 'updatedAt');
-  options.include = options.include || [];
-  options.include.push(
-    {
+     { model: Course },
+     {
       model: User,
       as: 'Admin',
       attributes: ['name'],
-      foreignKey: 'adminId'
-    },
-    {
-      model: Course
-    }
-  );
+      foreignKey: 'adminId',
+     },
+    ],
+   },
+  }
+ );
+});
+
+SubGroup.beforeFind(async (options) => {
+ options.attributes = options.attributes || {};
+ options.attributes.exclude = options.attributes.exclude || [];
+ options.attributes.exclude.push('createdAt', 'updatedAt');
+ options.include = options.include || [];
+ options.include.push(
+  {
+   model: User,
+   as: 'Admin',
+   attributes: ['name'],
+   foreignKey: 'adminId',
+  },
+  {
+   model: Course,
+  }
+ );
 });
 
 module.exports = {
-  User,
-  Role,
-  Course,
-  TeacherCourse,
-  Slot,
-  Appointment_Type,
-  SubGroup,
-  Replacement,
-  TeacherType
+ User,
+ Role,
+ Course,
+ TeacherCourse,
+ Slot,
+ Appointment_Type,
+ SubGroup,
+ Replacement,
+ TeacherType,
 };
