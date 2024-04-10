3efaeb01ba062039d302a58804dd8c4c5f59cad6
Sheeseburger <kekymber@gmail.com>
2024-04-10 12:01:03 +0300

    fixed creation and managment of spreadSheet

--- controllers/spreadsheet.controller.js
+++ controllers/spreadsheet.controller.js
@@ -2,7 +2,7 @@ const {User, Course, SubGroup, SubgroupMentor} = require('../models/relation');
 const {google} = require('googleapis');
 
 const spreadsheetId = process.env.SPREADSHEET_ID;
-
+let sheetId = 3; // !!!
 const loginToSheet = () => {
   // Create a new instance of GoogleAuth
   const auth = new google.auth.GoogleAuth({
@@ -28,7 +28,7 @@ const mergeCells = async (sheets, mergeRanges, columnStart, columnEnd) => {
                 {
                   mergeCells: {
                     range: {
-                      sheetId: 0,
+                      sheetId,
                       startRowIndex: mergeRange.start - 1,
                       endRowIndex: mergeRange.end - 1,
                       startColumnIndex: columnIndex,
@@ -47,7 +47,19 @@ const mergeCells = async (sheets, mergeRanges, columnStart, columnEnd) => {
 };
 
 const sheets = loginToSheet();
-
+sheets.spreadsheets
+  .get({
+    spreadsheetId
+  })
+  .then(response => {
+    const sheets = response.data.sheets;
+    const sheetInfo = sheets.find(sheet => sheet.properties.title === 'Sheet3');
+    sheetId = sheetInfo.properties.sheetId;
+    console.log('ID листа:', sheetId);
+  })
+  .catch(err => {
+    console.error('Ошибка:', err);
+  });
 exports.updateSheet = async (req, res, next) => {
   try {
     const users = await User.findAll({
@@ -64,72 +76,72 @@ exports.updateSheet = async (req, res, next) => {
       ]
     });
 
-    // const rows = [
-    //   [
-    //     'Mentor',
-    //     'Email',
-    //     'Course count',
-    //     'Subgroup count',
-    //     'Course',
-    //     'Subgroup',
-    //     'Type',
-    //     'Schedule'
-    //   ]
-    // ];
-    // let mergeRanges = [];
-    // let mergeSubgroupsRanges = [];
-    // let cursor = {start: 2, end: 2}; // 1 is header
-    // let subgroupCursor = {start: 2, end: 2};
-    // users.forEach(user => {
-    //   cursor.start = cursor.end;
-    //   subgroupCursor.start = cursor.start;
-    //   subgroupCursor.end = cursor.end;
-    //   let rowValues = [];
-    //   let subGroupLen = user.MentorSubGroups.length;
-    //   let coursesLen = user.teachingCourses.length;
-    //   rowValues[0] = user.name;
-    //   rowValues[1] = user.email;
-    //   rowValues[2] = coursesLen;
-    //   rowValues[3] = subGroupLen;
-    //   user.teachingCourses.forEach(course => {
-    //     subgroupCursor.start = subgroupCursor.end; // every new course must be new group of subgroups
+    const rows = [
+      [
+        'Mentor',
+        'Email',
+        'Course count',
+        'Subgroup count',
+        'Course',
+        'Subgroup',
+        'Type',
+        'Schedule'
+      ]
+    ];
+    let mergeRanges = [];
+    let mergeSubgroupsRanges = [];
+    let cursor = {start: 2, end: 2}; // 1 is header
+    let subgroupCursor = {start: 2, end: 2};
+    users.forEach(user => {
+      cursor.start = cursor.end;
+      subgroupCursor.start = cursor.start;
+      subgroupCursor.end = cursor.end;
+      let rowValues = [];
+      let coursesLen = user.teachingCourses.length;
+      let subGroupLen = user.mentorSubgroups.length;
+      rowValues[0] = user.name;
+      rowValues[1] = user.email;
+      rowValues[2] = coursesLen;
+      rowValues[3] = subGroupLen;
+      user.teachingCourses.forEach(course => {
+        subgroupCursor.start = subgroupCursor.end; // every new course must be new group of subgroups
 
-    //     rowValues[4] = course.name;
-    //     const subgroups = user.MentorSubGroups.filter(el => el.CourseId === course.id);
+        rowValues[4] = course.name;
+        const subgroups = user.mentorSubgroups.filter(el => el.CourseId === course.id);
 
-    //     if (subgroups.length > 0) {
-    //       subgroups.forEach(group => {
-    //         rowValues[5] = group.name;
-    //         rowValues[6] = 'type';
-    //         rowValues[7] = group.schedule;
-    //         rows.push([...rowValues]);
-    //         cursor.end += 1;
-    //         subgroupCursor.end += 1;
-    //       });
-    //       mergeSubgroupsRanges.push({start: subgroupCursor.start, end: subgroupCursor.end});
-    //     } else {
-    //       for (let i = 5; i <= 7; i++) rowValues[i] = null;
-    //       rows.push([...rowValues]);
-    //       cursor.end += 1;
-    //     }
-    //   });
-    //   mergeRanges.push({start: cursor.start, end: cursor.end});
-    // });
+        if (subgroups.length > 0) {
+          subgroups.forEach(group => {
+            rowValues[5] = group.name;
+            rowValues[6] = 'type';
+            rowValues[7] = group?.SubgroupMentor.schedule;
+            rows.push([...rowValues]);
+            cursor.end += 1;
+            subgroupCursor.end += 1;
+          });
+          mergeSubgroupsRanges.push({start: subgroupCursor.start, end: subgroupCursor.end});
+        } else {
+          for (let i = 5; i <= 7; i++) rowValues[i] = null;
+          rows.push([...rowValues]);
+          cursor.end += 1;
+        }
+      });
+      mergeRanges.push({start: cursor.start, end: cursor.end});
+    });
 
-    // const resource = {
-    //   spreadsheetId,
-    //   range: 'Sheet3',
-    //   valueInputOption: 'RAW',
-    //   resource: {values: rows}
-    // };
-    // await sheets.spreadsheets.values.update(resource);
-    // mergeCells(sheets, mergeRanges, 0, 3);
-    // mergeCells(sheets, mergeSubgroupsRanges, 4, 4);
+    const resource = {
+      spreadsheetId,
+      range: 'Sheet3',
+      valueInputOption: 'RAW',
+      resource: {values: rows}
+    };
+    await sheets.spreadsheets.values.update(resource);
+    mergeCells(sheets, mergeRanges, 0, 3);
+    mergeCells(sheets, mergeSubgroupsRanges, 4, 4);
 
     // Return the URL of the created spreadsheet
     res.json({
       spreadsheetUrl: `https://docs.google.com/spreadsheets/d/${spreadsheetId}`,
-      users,
+      // users,
       message: 'updated spreadsheet'
     });
   } catch (error) {
@@ -147,7 +159,7 @@ exports.resize = async (req, res, next) => {
           {
             autoResizeDimensions: {
               dimensions: {
-                sheetId: 0,
+                sheetId,
                 dimension: 'COLUMNS',
                 startIndex: 0,
                 endIndex: 8 // Adjust this based on the number of columns
@@ -169,19 +181,9 @@ exports.resize = async (req, res, next) => {
 
 exports.addBorders = async (req, res, next) => {
   try {
-    const border = {
-      style: 'SOLID',
-      width: 1,
-      color: {
-        red: 0,
-        green: 0,
-        blue: 0,
-        alpha: 1
-      }
-    };
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId,
-      range: 'A:H'
+      range: `Sheet3`
     });
 
     const values = response.data.values;
@@ -208,7 +210,7 @@ exports.addBorders = async (req, res, next) => {
             {
               updateBorders: {
                 range: {
-                  sheetId: 0,
+                  sheetId,
                   startRowIndex: 0,
                   endRowIndex: lastRow,
                   startColumnIndex: 0,
