f0649e861100f01f1dea755527150e87cf8cad02
Sheeseburger <kekymber@gmail.com>
2024-06-11 15:58:06 +0300

    updated structure of slack bot

--- utils/slackBot.js
+++ utils/slackBot.js
@@ -6,17 +6,17 @@ const app = new App({
   signingSecret: '39637d6b9e3a866708e66529adb214b5'
 });
 
-// Обработка упоминаний бота в канале
-app.event('app_mention', async ({event, say}) => {
-  await say(`Привет, <@${event.user}>! Как я могу помочь?`);
-});
-
-// Обработка сообщений в канале
-app.message(/привет/i, async ({message, say}) => {
-  await say(`Привет, <@${message.user}>!`);
-});
-
 (async () => {
   await app.start(3001);
+  // Обработка упоминаний бота в канале
+  app.event('app_mention', async ({event, say}) => {
+    await say(`Привет, <@${event.user}>! Как я могу помочь?`);
+  });
+
+  // Обработка сообщений в канале
+  app.message('hello', async ({message, say}) => {
+    await say(`Привет, <@${message.user}>!`);
+  });
+
   console.log('⚡️ Slack бот запущен на порту 3001');
 })();
