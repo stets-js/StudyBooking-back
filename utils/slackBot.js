f428a28a781f6a54f43e10b3e4969823f522b50c
Sheeseburger <kekymber@gmail.com>
2024-06-11 17:19:34 +0300

    trying to merge express + slack

--- utils/slackBot.js
+++ utils/slackBot.js
@@ -1,13 +1,15 @@
-const {App} = require('@slack/bolt');
+const {App, ExpressReceiver} = require('@slack/bolt');
 
+const receiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
 // Инициализация приложения с использованием токена и Signing Secret
 const slackApp = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  receiver
 });
-
 (async () => {
-  await slackApp.start(3001);
+  await slackApp.start();
   // Обработка упоминаний бота в канале
   slackApp.event('app_mention', async ({event, say}) => {
     await say(`Привет, <@${event.user}>! Как я могу помочь?`);
