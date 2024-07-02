8062dd858127d36e50320065e2a2cc63b767c4e6
Sheeseburger <kekymber@gmail.com>
2024-07-02 12:32:10 +0300

    not logging error routes

--- utils/logger.js
+++ utils/logger.js
@@ -6,6 +6,10 @@ const logger = (tokens, req, res) => {
   if (req.method === 'GET' && !notIgnoringGets.includes(tokens.url(req, res).split('?')[0])) {
     return null;
   }
+  const status = tokens.status(req, res);
+  if (status < 200 || status >= 300) {
+    return null;
+  }
   Logs.create({
     userId: req?.user?.id || null,
     path: tokens.url(req, res),
