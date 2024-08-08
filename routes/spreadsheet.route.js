8f1a261e1262316bd8a5e7fa03a8d8e0f42af0b7
Sheeseburger <kekymber@gmail.com>
2024-08-08 11:26:43 +0300

    update spreadsheet.route

--- routes/spreadsheet.route.js
+++ routes/spreadsheet.route.js
@@ -4,14 +4,14 @@ const authController = require('../controllers/auth.controller');
 const spreadsheetController = require('../controllers/spreadsheet.controller');
 
 const router = express.Router();
+router.get('/activityByCourse', spreadsheetController.getActivityStatsByCourse);
+router.get('/activity', spreadsheetController.getActivityStats);
 router
   .route('/:id/reports/:sheetId')
   .get(spreadsheetController.fetchReportsFromSheets)
   .post(spreadsheetController.AddReportsToDB);
 router.get('/:id', spreadsheetController.getAllSheets);
 
-router.get('/activity', spreadsheetController.getActivityStats);
-router.get('/activityByCourse', spreadsheetController.getActivityStatsByCourse);
 router.use(authController.protect, authController.allowedTo(['administrator', 'superAdmin']));
 router.route('/').get(spreadsheetController.updateSheet);
 router.route('/borders').get(spreadsheetController.addBorders);
