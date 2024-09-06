40337aef97bcefee52a48e5fc9f543f07b1ba22b
Sheeseburger <kekymber@gmail.com>
2024-09-06 13:59:07 +0300

    added unique condition

--- models/report.model.js
+++ models/report.model.js
@@ -4,56 +4,67 @@ const User = require('./user.model');
 const {Course} = require('./course.model');
 const {SubGroup} = require('./subgroup.model');
 
-const Report = sequelize.define('Reports', {
-  mentorId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: User,
-      key: 'id'
+const Report = sequelize.define(
+  'Reports',
+  {
+    mentorId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id'
+      }
+    },
+    courseId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Course,
+        key: 'id'
+      }
+    },
+    course: {type: DataTypes.STRING},
+    date: {
+      type: DataTypes.DATEONLY,
+      defaultValue: new Date()
+    },
+    mark1: {
+      type: DataTypes.INTEGER,
+      defaultValue: 100
+    },
+    mark2: {
+      type: DataTypes.INTEGER,
+      defaultValue: 100
+    },
+    crit_error: {
+      type: DataTypes.INTEGER,
+      defaultValue: 100
+    },
+
+    total: {
+      type: DataTypes.INTEGER,
+      defaultValue: 100
+    },
+    rating: {
+      type: DataTypes.STRING(1)
+    },
+    report_rating: {type: DataTypes.STRING(1)},
+    link: {type: DataTypes.STRING},
+    sheetName: {
+      type: DataTypes.STRING
+    },
+
+    status: {
+      type: DataTypes.ENUM('Погоджено', 'Погодження', 'Апеляція'),
+      defaultValue: 'Погодження'
     }
   },
-  courseId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: Course,
-      key: 'id'
-    }
-  },
-  course: {type: DataTypes.STRING},
-  date: {
-    type: DataTypes.DATEONLY,
-    defaultValue: new Date()
-  },
-  mark1: {
-    type: DataTypes.INTEGER,
-    defaultValue: 100
-  },
-  mark2: {
-    type: DataTypes.INTEGER,
-    defaultValue: 100
-  },
-  crit_error: {
-    type: DataTypes.INTEGER,
-    defaultValue: 100
-  },
-
-  total: {
-    type: DataTypes.INTEGER,
-    defaultValue: 100
-  },
-  rating: {
-    type: DataTypes.STRING(1)
-  },
-  report_rating: {type: DataTypes.STRING(1)},
-  link: {type: DataTypes.STRING},
-  sheetName: {
-    type: DataTypes.STRING
-  },
-
-  status: {
-    type: DataTypes.ENUM('Погоджено', 'Погодження', 'Апеляція'),
-    defaultValue: 'Погодження'
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ['mentorId', 'sheetName']
+      }
+    ]
   }
-});
+);
 
 module.exports = Report;
