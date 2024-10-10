d5b931456e9077adc94f7ea690a1ee5503e332d9
Sheeseburger <kekymber@gmail.com>
2024-10-10 16:44:03 +0300

    updated blocks generator

--- utils/slack-block.js
+++ utils/slack-block.js
@@ -6,54 +6,52 @@ const generateBlockConfirmation = (
   schedule,
   adminId
 ) => {
-  return {
-    blocks: [
-      {
-        type: 'header',
-        text: {
-          type: 'plain_text',
-          text: 'Вам призначено новий потік:',
-          emoji: true
-        }
-      },
-      {
-        type: 'section',
-
-        fields: [
-          {
-            type: 'mrkdwn',
-            text: `Назва:\n*${subgroupName}*`
-          },
-
-          {
-            type: 'mrkdwn',
-            text: `Курс:\n*${courseName}*`
-          },
-          {
-            type: 'mrkdwn',
-            text: `Призначив:\n*<@${adminId}>*`
-          }
-        ]
-      },
-      {
-        type: 'section',
-        fields: [
-          {
-            type: 'mrkdwn',
-            text: `*Дата початку:*\n${startDate}`
-          },
-          {
-            type: 'mrkdwn',
-            text: `*Кінцева дата:*\n${endDate}`
-          },
-
-          {
-            type: 'mrkdwn',
-            text: `*Графік:*\n ${schedule.replaceAll(',', '\n')}`
-          }
-        ]
+  return [
+    {
+      type: 'header',
+      text: {
+        type: 'plain_text',
+        text: 'Вам призначено новий потік:',
+        emoji: true
       }
-    ]
-  };
+    },
+    {
+      type: 'section',
+
+      fields: [
+        {
+          type: 'mrkdwn',
+          text: `Назва:\n*${subgroupName}*`
+        },
+
+        {
+          type: 'mrkdwn',
+          text: `Курс:\n*${courseName}*`
+        },
+        {
+          type: 'mrkdwn',
+          text: `Призначив:\n*<@${adminId}>*`
+        }
+      ]
+    },
+    {
+      type: 'section',
+      fields: [
+        {
+          type: 'mrkdwn',
+          text: `*Дата початку:*\n${startDate}`
+        },
+        {
+          type: 'mrkdwn',
+          text: `*Кінцева дата:*\n${endDate}`
+        },
+
+        {
+          type: 'mrkdwn',
+          text: `*Графік:*\n ${schedule.replaceAll(',', '\n')}`
+        }
+      ]
+    }
+  ];
 };
 module.exports = {generateBlockConfirmation};
